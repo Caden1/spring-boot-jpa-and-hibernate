0cfbf6fa9a3c241ee6555fe2946b327319117b81
Caden Barton <cadenbarton@Cadens-MacBook-Air.local>
2024-04-08 18:01:08 -0600

    Used spring data jpa to make database interactions way easier

--- src/main/java/com/learn/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/learn/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import com.learn.springboot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
 import com.learn.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.learn.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
@@ -13,19 +14,30 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 //	@Autowired
 //	private CourseJdbcRepository repository;
 	
+//	@Autowired
+//	private CourseJpaRepository repository;
+	
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1, "Learn AWS Now - JPA", "in28minutes"));
-		repository.insert(new Course(2, "Learn Azure Now - JPA", "in28minutes"));
-		repository.insert(new Course(3, "Learn DevOps Now - JPA", "in28minutes"));
+		repository.save(new Course(1, "Learn AWS Now - JPA", "in28minutes"));
+		repository.save(new Course(2, "Learn Azure Now - JPA", "in28minutes"));
+		repository.save(new Course(3, "Learn DevOps Now - JPA", "in28minutes"));
 		
-		repository.deleteById(1);
+		repository.deleteById(1l);
 		
-		System.out.println(repository.findById(2));
-		System.out.println(repository.findById(3));
+		System.out.println(repository.existsById(1l));
+		System.out.println(repository.existsById(2l));
+		
+		System.out.println(repository.findById(2l));
+		System.out.println(repository.findById(3l));
+		
+		System.out.println(repository.findByAuthor("in28minutes"));
+		System.out.println(repository.findByAuthor(""));
+		
+		System.out.println(repository.findByName("Learn Azure Now - JPA"));
 	}
 	
 }
